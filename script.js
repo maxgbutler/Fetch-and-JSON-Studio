e784f4467db988bd709814805c7051cdccede0e7
Max Butler <maxgbutler003@gmail.com>
2023-03-14 23:12:28 -0500

    makes everything work

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener('load', function() {
+
+    const container = document.getElementById('container');
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data)
+
+            for (let i = 0; i < data.length; i++) {
+                let astronaut = data[i];
+                container.innerHTML += 
+                `<div class="astronaut">
+                    <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>${astronaut.hoursInSpace}</li>
+                        <li>${astronaut.active}</li>
+                        <li>${astronaut.skills}</li>
+                    </ul>
+                    </div>
+                <img class="avatar" src="${astronaut.picture}">
+                </div>`
+            }
+        });
+    });
+});
+
+
+
+  
